7f2b67a4bff53a0e93c4f62f27d4038f25108343
PruthuviHasanjana <2017e009@eng.jfn.ac.lk>
2021-03-29 23:02:17 +0530

    day 05 lesson 01 is added.

--- lib/widgets/day04Lesson02.dart
+++ lib/widgets/day04Lesson02.dart
@@ -14,9 +14,8 @@ class Day04Lesson02 extends StatelessWidget {
         padding: EdgeInsets.all(getProportionateScreenHeight(15)),
         child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
           Divider(),
-          //main titles
           Text(
-            'Update your Mr English app',
+            'There is/ There are',
             style: TextStyle(
               fontWeight: FontWeight.bold,
               fontSize: titleSize,
@@ -25,6 +24,350 @@ class Day04Lesson02 extends StatelessWidget {
             ),
           ),
           Divider(),
+          Text(
+            'යම් ස්ථානයක යම් දෙයක් හෝ යම් ස්ථානයක යම් කෙනෙක් සිටින බව ප්‍රකාශ කිරීමට there is/ there are යොදයි. මෙම grammer කොටස ඉංග්‍රීසි විභාග සඳහා පෙනී සිටින ඔබ හට ඉතාමත් වැදගත් වනු ඇත. යම් රුපයක් විස්තර කිරීමේදී මේ there is/ there are ක්‍රමය ඉතා වැදගත් වේ.',
+            style: TextStyle(
+              fontSize: size,
+              color: Colors.black,
+            ),
+          ),
+          Text(
+            'ඔබගේ පහසුව සඳහා වාක්‍යක් තැනීමට අවශ්‍ය සුත්‍රයක් පහතින් දක්වා ඇත.',
+            style: TextStyle(
+              fontSize: size,
+              color: Colors.black,
+            ),
+          ),
+          Text(
+            ' ',
+            style: TextStyle(
+              fontSize: size,
+              color: Colors.black,
+            ),
+          ),
+          Text(
+            'There is/ are + what (මොකක්ද) + where (කොහේද) ',
+            style: TextStyle(
+              fontSize: size,
+              color: Colors.black,
+            ),
+          ),
+          Text(
+            ' ',
+            style: TextStyle(
+              fontSize: size,
+              color: Colors.black,
+            ),
+          ),
+          Text(
+            'අපි කතා කරන දෙය sigular නම් there is ද plural නම් there are ද යොදයි.',
+            style: TextStyle(
+              fontSize: size,
+              color: Colors.black,
+            ),
+          ),
+          Text(
+            '  ',
+            style: TextStyle(
+              fontSize: size,
+              color: Colors.blue,
+            ),
+          ),
+          Text(
+            'Example: ',
+            style: TextStyle(
+              fontSize: size,
+              color: Colors.black,
+            ),
+          ),
+          Text(
+            '  අපේ පාසලේ පුස්තකාලයක් තියනවා.',
+            style: TextStyle(
+              fontSize: size,
+              color: Colors.blue,
+            ),
+          ),
+          Text(
+            'මෙහිදී අප කතා කරන්නේ පුස්තකාලය (Library) ගැනයි.එය තියෙන්නේ අප පාසල තුලය.(in our school) Library singular බැවින් there is යොදයි. ',
+            style: TextStyle(
+              fontSize: size,
+              color: Colors.black,
+            ),
+          ),
+          Text(
+            '  There is a library in our school.',
+            style: TextStyle(
+              fontSize: size,
+              color: Colors.blue,
+            ),
+          ),
+          Text(
+            '',
+            style: TextStyle(
+              fontSize: size,
+              color: Colors.black,
+            ),
+          ),
+          Text(
+            '  මේ පින්තුරයේ ළමයි 10ක් සිටියි.',
+            style: TextStyle(
+              fontSize: size,
+              color: Colors.blue,
+            ),
+          ),
+          Text(
+            'මෙහිදී අප කතා කරන්නේ ළමයි 10 දෙනා ගැනය. එම ළමුන් 10 දෙනා සිටින්නේ මේ පින්තුරයේය. ten children plural noun වන බැවින් there are යෙදේ.',
+            style: TextStyle(
+              fontSize: size,
+              color: Colors.black,
+            ),
+          ),
+          Text(
+            '  There are ten children in this picture.',
+            style: TextStyle(
+              fontSize: size,
+              color: Colors.blue,
+            ),
+          ),
+          Text(
+            '  ',
+            style: TextStyle(
+              fontSize: size,
+              color: Colors.blue,
+            ),
+          ),
+          Text(
+            'Example: ',
+            style: TextStyle(
+              fontSize: size,
+              color: Colors.black,
+            ),
+          ),
+          Text(
+            '  There is a tank in our village.',
+            style: TextStyle(
+              fontSize: size,
+              color: Colors.blue,
+            ),
+          ),
+          Text(
+            '  (අපේ ගමේ වැවක් තියනවා.)',
+            style: TextStyle(
+              fontSize: size,
+              color: Colors.blue,
+            ),
+          ),
+          Text(
+            '  ',
+            style: TextStyle(
+              fontSize: size,
+              color: Colors.blue,
+            ),
+          ),
+          Text(
+            '  There are six members in my family.',
+            style: TextStyle(
+              fontSize: size,
+              color: Colors.blue,
+            ),
+          ),
+          Text(
+            '  (මගේ පවුලේ සාමාජිකයින් හය දෙනෙක් සිටිනවා.)',
+            style: TextStyle(
+              fontSize: size,
+              color: Colors.blue,
+            ),
+          ),
+          Text(
+            '  ',
+            style: TextStyle(
+              fontSize: size,
+              color: Colors.blue,
+            ),
+          ),
+          Text(
+            '  There is a girl in this picture.',
+            style: TextStyle(
+              fontSize: size,
+              color: Colors.blue,
+            ),
+          ),
+          Text(
+            '  (මේ පින්තුරයේ ගැහැණු ළමයෙක් සිටිනවා.)',
+            style: TextStyle(
+              fontSize: size,
+              color: Colors.blue,
+            ),
+          ),
+          Text(
+            '  ',
+            style: TextStyle(
+              fontSize: size,
+              color: Colors.blue,
+            ),
+          ),
+          Divider(),
+          Text(
+            'Negative form',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: titleSize,
+              color: Colors.black,
+              decoration: TextDecoration.underline,
+            ),
+          ),
+          Divider(),
+          Text(
+            'මෙහිදී සෑම විටම be verbs වලට not යෙදේ.',
+            style: TextStyle(
+              fontSize: size,
+              color: Colors.black,
+            ),
+          ),
+          Text(
+            '  is not, are not',
+            style: TextStyle(
+              fontSize: size,
+              color: Colors.black,
+            ),
+          ),
+          Text(
+            'Example: ',
+            style: TextStyle(
+              fontSize: size,
+              color: Colors.black,
+            ),
+          ),
+          Text(
+            '  There is not a toilet in this class.',
+            style: TextStyle(
+              fontSize: size,
+              color: Colors.blue,
+            ),
+          ),
+          Text(
+            '  (මේ පන්තියේ වැසිකිලියක් නැත.)',
+            style: TextStyle(
+              fontSize: size,
+              color: Colors.blue,
+            ),
+          ),
+          Text(
+            '',
+            style: TextStyle(
+              fontSize: size,
+              color: Colors.black,
+            ),
+          ),
+          Text(
+            '  There are not hotels in our village.',
+            style: TextStyle(
+              fontSize: size,
+              color: Colors.blue,
+            ),
+          ),
+          Text(
+            '  (අපේ ගමේ හෝටල් නැත.)',
+            style: TextStyle(
+              fontSize: size,
+              color: Colors.blue,
+            ),
+          ),
+          Text(
+            '',
+            style: TextStyle(
+              fontSize: size,
+              color: Colors.black,
+            ),
+          ),
+          Divider(),
+          Text(
+            'Past and future',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: titleSize,
+              color: Colors.black,
+              decoration: TextDecoration.underline,
+            ),
+          ),
+          Divider(),
+          Text(
+            'ඉහත අකාරයේ ප්‍රකාශ අතීත හා අනාගත කාලයෙන් කතා කිරීමේදී be verbs පමණක් වෙනස් වේ. be verbs ගැන ඔබට සම්පුර්ණ අවබෝධයක් අවශ්‍යනම් අපගේ be verbs පාඩම් මාලාව කියවන්න.',
+            style: TextStyle(
+              fontSize: size,
+              color: Colors.black,
+            ),
+          ),
+          Text(
+            '',
+            style: TextStyle(
+              fontSize: size,
+              color: Colors.black,
+            ),
+          ),
+          Text(
+            'past - was/ were ',
+            style: TextStyle(
+              fontSize: size,
+              color: Colors.black,
+            ),
+          ),
+          Text(
+            'future - will be',
+            style: TextStyle(
+              fontSize: size,
+              color: Colors.black,
+            ),
+          ),
+          Text(
+            '',
+            style: TextStyle(
+              fontSize: size,
+              color: Colors.black,
+            ),
+          ),
+          Text(
+            'Example: ',
+            style: TextStyle(
+              fontSize: size,
+              color: Colors.black,
+            ),
+          ),
+          Text(
+            '  There was a student called Nishantha in my class.',
+            style: TextStyle(
+              fontSize: size,
+              color: Colors.blue,
+            ),
+          ),
+          Text(
+            '  (මගේ පන්තියේ නිශාන්ත කියලා සිසුවෙක් හිටියා.)',
+            style: TextStyle(
+              fontSize: size,
+              color: Colors.blue,
+            ),
+          ),
+          Text(
+            '',
+            style: TextStyle(
+              fontSize: size,
+              color: Colors.black,
+            ),
+          ),
+          Text(
+            '  There will be ten members in your group.',
+            style: TextStyle(
+              fontSize: size,
+              color: Colors.blue,
+            ),
+          ),
+          Text(
+            '  ඔබගේ කණ්ඩායමේ සාමාජිකයින් දස දෙනෙකු සිටීවි.',
+            style: TextStyle(
+              fontSize: size,
+              color: Colors.blue,
+            ),
+          ),
         ]),
       ),
     );
